5a324b5de978db0b6eabcb54d2a5ef3f69cf1118
majestymewtwo <majestymewtwo@gmail.com>
2023-08-20 15:41:03 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      grey: "#EEE2DE",
+      peach: "#EA906C",
+      maroon: "#900C3F",
+      navyBlue: "#2B2A4C",
+      indigo: "#504099",
+      violet: "#4D3C77",
+      pink: "#FE7BE5",
+      purple: "#974EC3",
+      yellow: "#F8DE22",
+      offWhite: "#EAEAEA",
+    },
+    extend: {
+      fontFamily: {
+        "old-standard": ["Old Standard TT", "serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        "roboto-condensed": ["Roboto Condensed", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
